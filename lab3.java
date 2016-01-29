1312eb796f17ef4f07a48b635b4e7e3880678eb0
tkl96 <til41@pitt.edu>
2016-01-29 09:22:16 -0500

    lab3
    java

---
+++ lab3.java
@@ -0,0 +1,19 @@
+public class lab3{
+	
+	public static void main(String[] args) {
+	double d=10;
+	double p=0;
+
+	int n=0;
+	while (d > 0.0){
+		n=n+1;
+		p+= d/2;
+		d= 10-p;
+		
+		System.out.println(p);
+
+	}
+
+	}
+}
+	
